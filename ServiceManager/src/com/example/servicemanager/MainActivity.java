0bc7c64402eca87ed7d4723406eaf838b75bc7f7
1956766863@qq.com <1956766863@qq.com>
2016-07-12 17:05:21 +0800

    添加

--- ServiceManager/src/com/example/servicemanager/MainActivity.java
+++ ServiceManager/src/com/example/servicemanager/MainActivity.java
@@ -1,16 +1,61 @@
 package com.example.servicemanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.servicemanager.bean.AppBeanInfo;
+import com.example.servicemanager.load.AppsAdapter;
+import com.example.servicemanager.load.LoadAppsTask;
+import com.example.servicemanager.load.OnObtainAppsInterface;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnObtainAppsInterface {
+	private ListView mListView;
+	private AppsAdapter adapter;
+	private List<AppBeanInfo> mAppBeans;
+	private ProgressDialog mProgressDialog;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		mListView = (ListView) findViewById(R.id.lv);
+		mAppBeans = new ArrayList<AppBeanInfo>();
+		adapter = new AppsAdapter(this, mAppBeans);
+		mListView.setAdapter(adapter);
+		mProgressDialog = new ProgressDialog(this, AlertDialog.THEME_HOLO_LIGHT);
+		mProgressDialog.setCancelable(false);
 		
+		new LoadAppsTask(this, this).run();
+	}
+
+	@Override
+	public void onObtainStart() {
+		// TODO Auto-generated method stub
+		mProgressDialog.setMessage("正在获取应用列表");
+		mProgressDialog.show();
+	}
+
+	@Override
+	public void onObtainFailed() {
+		// TODO Auto-generated method stub
+		mProgressDialog.dismiss();
+	}
+
+	@Override
+	public void onObtainSuccess(List<AppBeanInfo> appBeans, List<AppBeanInfo> activeBeans) {
+		// TODO Auto-generated method stub
+		mProgressDialog.dismiss();
+		mAppBeans.clear();
+		mAppBeans.addAll(appBeans);
+		adapter.notifyDataSetChanged();
 	}
 
 	
--- ServiceManager/src/com/example/servicemanager/load/AppsAdapter.java
+++ ServiceManager/src/com/example/servicemanager/load/AppsAdapter.java
@@ -1,21 +1,47 @@
 package com.example.servicemanager.load;
 
+import java.util.List;
+
+import com.example.servicemanager.R;
+import com.example.servicemanager.bean.AppBeanInfo;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AppsAdapter extends BaseAdapter{
+	private Context mContext;
+	private List<AppBeanInfo> appBeans;
+	private LayoutInflater mInflater;
+	private PackageManager packageManager;
+	
+	public AppsAdapter(Context context,List<AppBeanInfo> infos) {
+		// TODO Auto-generated constructor stub
+		mInflater = LayoutInflater.from(context);
+		packageManager = context.getPackageManager();
+		this.appBeans = infos;
+		this.mContext = context;
+		
+	}
 
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return appBeans.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		return null;
+		return appBeans.get(position);
 	}
 
 	@Override
@@ -27,7 +53,40 @@ public class AppsAdapter extends BaseAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		ViewHolder viewHolder = null;
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.row_app, null);
+			viewHolder = new ViewHolder();
+			viewHolder.mIvAppIcon = (ImageView) convertView.findViewById(R.id.iv_app_icon);
+			viewHolder.mTvAppName = (TextView) convertView.findViewById(R.id.tv_app_name);
+			viewHolder.mTvAppDate = (TextView) convertView.findViewById(R.id.tv_app_date);
+			viewHolder.mTvAppType = (TextView) convertView.findViewById(R.id.tv_app_type);
+			viewHolder.mTvAppPkg = (TextView) convertView.findViewById(R.id.tv_app_pkg);
+			convertView.setTag(viewHolder);
+		}else{
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+		AppBeanInfo info = appBeans.get(position);
+		viewHolder.mTvAppName.setText(info.app_name);
+		viewHolder.mTvAppType.setText(info.app_app_type);
+		viewHolder.mTvAppPkg.setText(info.app_pkg);
+		viewHolder.mTvAppDate.setText(DateFormat.format("yyyy-MM-dd hh:MM:ss", info.app_install_date));
+		try {
+			Drawable drawable = packageManager.getApplicationIcon(info.app_pkg);
+			viewHolder.mIvAppIcon.setImageDrawable(drawable);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return convertView;
+	}
+	
+	static class ViewHolder{
+		public ImageView mIvAppIcon;
+		public TextView mTvAppName;
+		public TextView mTvAppType;
+		public TextView mTvAppPkg;
+		public TextView mTvAppDate;
 	}
 
 }
